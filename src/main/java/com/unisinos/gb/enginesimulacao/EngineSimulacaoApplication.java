a0d1149d797e65cd3b9e37018120fd0b85985088
pablo_kleim <pablo.190777@gmail.com>
2022-05-15 12:22:15 -0300

    first commit

---
+++ src/main/java/com/unisinos/gb/enginesimulacao/EngineSimulacaoApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.gb.enginesimulacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineSimulacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineSimulacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisinos/gb/enginesimulacao/EngineSimulacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.gb.enginesimulacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineSimulacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
