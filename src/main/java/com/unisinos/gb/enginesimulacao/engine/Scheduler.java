465f1dc4d2f197d873d19448dd01b168017630cb
Cristiano Farias <cristiano@snotra.com.br>
2022-06-12 22:12:18 -0300

    ✔ Ajustes de métodos no simulate()

--- src/main/java/com/unisinos/gb/enginesimulacao/engine/Scheduler.java
+++ src/main/java/com/unisinos/gb/enginesimulacao/engine/Scheduler.java
@@ -1,176 +1,173 @@
 package com.unisinos.gb.enginesimulacao.engine;
 
-import com.unisinos.gb.enginesimulacao.model.entity.EntitySet;
-import com.unisinos.gb.enginesimulacao.model.event.Event;
-import com.unisinos.gb.enginesimulacao.model.process.Process;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.unisinos.gb.enginesimulacao.model.entity.EntitySet;
+import com.unisinos.gb.enginesimulacao.model.event.Event;
+import com.unisinos.gb.enginesimulacao.model.process.Process;
+
 public class Scheduler {
 
-    public static double meanValue = 2;
-    public static double stdDeviationValue = 5;
-    public static double minValue = 1;
-    public static double maxValue = 6;
+	public static double meanValue = 2;
+	public static double stdDeviationValue = 5;
+	public static double minValue = 1;
+	public static double maxValue = 6;
 
-    private Double tempo = 0.0;
-    private int id = 0;
+	private Double tempo = 0.0;
+	private int id = 0;
 
-    private int contChegada = 0;
-    private int contSaida = 0;
+	private int contChegada = 0;
+	private int contSaida = 0;
 
-    // Adicionado listas
-    private final List<Event> eventosAgendados = new ArrayList<>();
-    private final List<EntitySet> entitySetList = new ArrayList<>();
+	// Adicionado listas
+	private final List<Event> eventosAgendados = new ArrayList<>();
+	private final List<EntitySet> entitySetList = new ArrayList<>();
 
-    public void addEntitySet(EntitySet entitySet) {
-        this.entitySetList.add(entitySet);
-    }
+	public void addEntitySet(EntitySet entitySet) {
+		this.entitySetList.add(entitySet);
+	}
 
-    public List<EntitySet> entitySetList() {
-        return this.entitySetList;
-    }
+	public List<EntitySet> entitySetList() {
+		return this.entitySetList;
+	}
 
-    public Double getTempo() {
-        return tempo;
-    }
+	public Double getTempo() {
+		return tempo;
+	}
 
-    // disparo de eventos e processos =============================================
+	// disparo de eventos e processos =============================================
 
-    public void scheduleNow(Event event) {
-        event.setTime(this.tempo);
-        eventosAgendados.add(event);
-    }
+	public void scheduleNow(Event event) {
+		event.setTime(this.tempo);
+		eventosAgendados.add(event);
+	}
 
-    public void scheduleIn(Event event, Double timeToEvent) {
-        event.setTime(tempo + timeToEvent);
-        eventosAgendados.add(event);
-    }
+	public void scheduleIn(Event event, Double timeToEvent) {
+		event.setTime(tempo + timeToEvent);
+		eventosAgendados.add(event);
+	}
 
-    public void scheduleAt(Event event, Double absoluteTime) {
-        event.setTime(absoluteTime);
-        eventosAgendados.add(event);
-    }
+	public void scheduleAt(Event event, Double absoluteTime) {
+		event.setTime(absoluteTime);
+		eventosAgendados.add(event);
+	}
 
-    public void startProcessNow(Process process) {
-        process.setTime(tempo);
+	public void startProcessNow(Process process) {
+		process.setTime(tempo);
 //		processosAgendados.add(process);
-    }
+	}
 
-    public void startProcessIn(Process process, Double timeToStart) {
-        process.setTime(tempo + timeToStart);
+	public void startProcessIn(Process process, Double timeToStart) {
+		process.setTime(tempo + timeToStart);
 //		processosAgendados.add(process);
-    }
+	}
 
-    public void startProcessAt(Process process, Double absoluteTime) {
-        process.setTime(absoluteTime);
+	public void startProcessAt(Process process, Double absoluteTime) {
+		process.setTime(absoluteTime);
 //		processosAgendados.add(process);
-    }
+	}
 
-    /**
-     *  se a abordagem para especificação da passagem de tempo nos processos for
-     * explícita
-     */
-    public void waitFor(long time) {
+	/**
+	 *  se a abordagem para especificação da passagem de tempo nos processos for
+	 * explícita
+	 */
+	public void waitFor(long time) {
 
-    }
+	}
 
-    // controlando tempo de execução ===============================================
+	// controlando tempo de execução ===============================================
 
-    /**
-     * executa até esgotar o modelo, isto é, até a engine não ter mais nada para
-     * processar (FEL vazia, i.e., lista de eventos futuros vazia)
-     */
-    public void simulate() {
-        while (!eventosAgendados.isEmpty()) {
-            var proximoCiclo = getProximoCiclo();
-            var eventosDoCiclo = eventosAgendados.stream().filter(it -> it.getTempo() <= proximoCiclo).collect(Collectors.toList());
+	/**
+	 * executa até esgotar o modelo, isto é, até a engine não ter mais nada para
+	 * processar (FEL vazia, i.e., lista de eventos futuros vazia)
+	 */
+	public void simulate() {
+		while (!eventosAgendados.isEmpty()) {
+			var proximoCiclo = getProximoCiclo();
+			var eventosDoCiclo = eventosAgendados.stream().filter(it -> it.getTempo() <= proximoCiclo).collect(Collectors.toList());
+			eventosDoCiclo.forEach(menorEvento -> {
+				menorEvento.execute();
+				// Adicionado pois eventos que forem do tipo Processo não podem ser removidos
+				if (!(menorEvento instanceof Process)) {
+					eventosAgendados.remove(menorEvento);
+				}
+			});
+			this.tempo = proximoCiclo;
+		}
+		System.out.println("Todos eventos processados.");
+	}
 
+	/*
+	 * executa somente uma primitiva da API e interrompe execução; por ex.: dispara
+	 * um evento e para; insere numa fila e para, etc.
+	 */
+	public void simulateOneStep() {
+		// this.eventosAgendados.get(0).execute();
+	}
 
-            eventosDoCiclo.forEach(menorEvento -> {
-                menorEvento.execute();
-                eventosAgendados.remove(menorEvento);
-            });
-            this.tempo = proximoCiclo;
-        }
-        System.out.println("Todos eventos processados.");
-    }
+	public void simulateBy(long duration) {
+		while (this.getTempo() <= duration) {
+			// simula
+		}
+	}
 
-    /*
-     * executa somente uma primitiva da API e interrompe execução; por ex.: dispara
-     * um evento e para; insere numa fila e para, etc.
-     */
-    public void simulateOneStep() {
-        //    	this.eventosAgendados.get(0).execute();
-    }
+	public void simulateUntil(long absoluteTime) {
+		while (this.getTempo() < absoluteTime) {
+			// simula
+		}
+	}
 
-    public void simulateBy(long duration) {
-        while (this.getTempo() <= duration) {
-            // simula
-        }
-    }
+	// criação, destruição e acesso para componentes
+	// ===============================================
 
-    public void simulateUntil(long absoluteTime) {
-        while (this.getTempo() < absoluteTime) {
-            // simula
-        }
-    }
+	public Integer generateId() {
+		return ++id;
+	}
 
-    // criação, destruição e acesso para componentes
-    // ===============================================
+	/*
+	 * retorna referência para instancia de Event
+	 */
+	public Event getEvent(Integer eventId) throws Exception {
+		return this.eventosAgendados.stream().filter(e -> e.getId().intValue() == eventId.intValue()).findAny().orElse(null);
+	}
 
+	/*
+	 * retorna referência para instancia de EntitySet
+	 */
+	public EntitySet getEntitySet(Integer id) throws Exception {
+		return this.entitySetList.stream().filter(esl -> esl.getId() == id.intValue()).findAny().orElse(null);
+	}
 
-    public Integer generateId() {
-        return ++id;
-    }
+	// random variates
 
-    /*
-     * retorna referência para instancia de Event
-     */
-    public Event getEvent(Integer eventId) throws Exception {
-        return this.eventosAgendados.stream().filter(e -> e.getId().intValue() == eventId.intValue()).findAny().orElse(null);
-    }
+	/**
+	 * Cria chegada na fila pelo tempo passado em minutos
+	 */
 
-    /*
-     * retorna referência para instancia de EntitySet
-     */
-    public EntitySet getEntitySet(Integer id) throws Exception {
-        return this.entitySetList.stream().filter(esl -> esl.getId() == id.intValue()).findAny().orElse(null);
-    }
+	public Double getProximoCiclo() {
+		return eventosAgendados.stream().min(Comparator.comparing(Event::getTempo)).orElseThrow().getTempo();
+	}
 
-    // random variates
+	// Alerta De Gambiarra!
+	public Optional<Double> getProximoProximoCiclo() {
+		return eventosAgendados.stream().filter(event -> event.getTempo() != this.tempo).min(Comparator.comparing(Event::getTempo)).map(Event::getTempo);
+	}
 
-    /**
-     * Cria chegada na fila pelo tempo passado em minutos
-     */
+	public void reAgendarProcessoProximoCiclo(Integer processID) {
+		getProximoProximoCiclo().ifPresent(it -> reAgendarProcesso(processID, it));
 
-    public Double getProximoCiclo() {
-        return eventosAgendados.stream().min(Comparator.comparing(Event::getTempo)).orElseThrow().getTempo();
-    }
+	}
 
-    //Alerta De Gambiarra!
-    public Optional<Double> getProximoProximoCiclo() {
-        return eventosAgendados.stream()
-                .filter(event -> event.getTempo() != this.tempo)
-                .min(Comparator.comparing(Event::getTempo))
-                .map(Event::getTempo);
-    }
-
-    public void reAgendarProcessoProximoCiclo(Integer processID) {
-        getProximoProximoCiclo().ifPresent(it -> reAgendarProcesso(processID, it));
-
-    }
-
-    public void reAgendarProcesso(Integer processID, Double time) {
-        Event eventProcess = eventosAgendados.stream().filter(proc -> proc.getId() == processID.intValue()).findAny().orElseThrow();
-        if (!(eventProcess instanceof Process)) {
-            throw new RuntimeException("Event " + eventProcess.getId() + " Nao e processo!!");
-        }
-        eventProcess.setTime(this.tempo + time);
-        eventosAgendados.add(eventProcess);
-    }
+	public void reAgendarProcesso(Integer processID, Double time) {
+		Event eventProcess = eventosAgendados.stream().filter(proc -> proc.getId() == processID.intValue()).findAny().orElseThrow();
+		if (!(eventProcess instanceof Process)) {
+			throw new RuntimeException("Event " + eventProcess.getId() + " Nao e processo!!");
+		}
+		eventProcess.setTime(this.tempo + time);
+		eventosAgendados.add(eventProcess);
+	}
 }
